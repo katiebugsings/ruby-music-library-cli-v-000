5aab395de441d188686c623419b4b4aee7511318
Katie Cassell <kcassell944@gmail.com>
2018-04-18 17:09:49 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -79,7 +79,7 @@ end
     input = gets.strip
     if genre = Genre.find_by_name(input)
       genre.songs.sort_by{|song|song.name}.each.with_index(1) do |song, index|
-        puts "#{index}. #{artist.name} - #{song.name}"
+        puts "#{index}. #{song.name} - #{song.artist.name}"
       end
     end
   end
