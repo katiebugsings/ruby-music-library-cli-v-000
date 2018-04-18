2b6ed0e22bf9bcffaa119be98fdb30d86d2afbe6
Katie Cassell <kcassell944@gmail.com>
2018-04-18 17:07:12 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -75,6 +75,13 @@ end
   end
 
   def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort_by{|song|song.name}.each.with_index(1) do |song, index|
+        puts "#{index}. #{song.name} - #{song.artist.name}"
+      end
+    end
   end
 
   def play_song
